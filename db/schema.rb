682d148aef207acffa163490387779bc15d25fbf
jaderswift <jades@manatts.com>
2019-10-22 14:00:15 -0500

    added admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_193347) do
+ActiveRecord::Schema.define(version: 2019_10_22_182826) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_193347) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
